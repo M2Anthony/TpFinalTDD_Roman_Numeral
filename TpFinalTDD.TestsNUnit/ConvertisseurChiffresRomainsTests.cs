26c168bddef34d30b5c45f5113197867f5b9bcc2
M2Anthony <anthony.douchet@hotmail.fr>
2024-01-26 13:52:22 +0100

    RED - ConversionVersEntier_Quand_chiffresRomains_EstVide_Alors_ArgumentException

--- TpFinalTDD.TestsNUnit/ConvertisseurChiffresRomainsTests.cs
+++ TpFinalTDD.TestsNUnit/ConvertisseurChiffresRomainsTests.cs
@@ -1,16 +1,25 @@
+using TpFinalTDD.Core;
+
 namespace TpFinalTDD.TestsNUnit
 {
     public class ConvertisseurChiffresRomainsTests
     {
+        private ConvertisseurChiffresRomains _convertisseur;
+
         [SetUp]
         public void Setup()
         {
+            _convertisseur = new ConvertisseurChiffresRomains();
         }
 
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+        public void ConversionVersEntier_Quand_chiffresRomains_EstVide_Alors_ArgumentException()
+        {            
+            // Assert & Act
+
+            Assert.Throws<ArgumentException>( () => _convertisseur.ConversionVersEntier("") );
+
         }
+        
     }
 }
